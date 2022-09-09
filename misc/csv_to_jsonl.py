98c0280da743485163114a81485d82ab04c31eeb
Fabien Roger <efab.roger@gmail.com>
2022-09-09 12:42:37 +0200

    Fix script

--- misc/csv_to_jsonl.py
+++ misc/csv_to_jsonl.py
@@ -1,11 +1,20 @@
 import json
+from optparse import Option
 import sys
+from typing import Optional
 
-import fire
+import fire  # type: ignore
 import pandas as pd
 
 
-def load(load_path, save_path, input_col, output_col, headers=None, accepted_outputs=None):
+def load(
+    load_path: str,
+    save_path: str,
+    input_col: str,
+    output_col: str,
+    headers: Optional[list[str]] = None,
+    accepted_outputs: Optional[list[str]] = None,
+):
     """Transform a csv dataset into a jsonl dataset.
 
     Example use: python misc/csv_to_jsonl.py my_data/twitter_validation.csv counterfactual_dataset_generator/data/examples/twitter-sentiment.jsonl Tweet_content Sentiment --headers ['Tweet_ID','Entity','Sentiment','Tweet_content'] --accepted_outputs ['Positive','Neutral','Negative']"""
@@ -13,7 +22,7 @@ def load(load_path, save_path, input_col, output_col, headers=None, accepted_out
     lines_written = 0
     with open(save_path, "w", encoding="utf-8") as outfile:
         for i, row in df.iterrows():
-            if row[output_col] in accepted_outputs:
+            if accepted_outputs is None or row[output_col] in accepted_outputs:
                 json_dict = {"input": row[input_col], "expected_output": row[output_col]}
                 json.dump(json_dict, outfile)
                 outfile.write("\n")
