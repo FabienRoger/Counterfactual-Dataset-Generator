f4ef155647b68488350f44bb708caab7e66ab77a
Fabien Roger <efab.roger@gmail.com>
2022-09-11 10:08:55 +0200

    Make it a module

---
+++ counterfactual_dataset_generator/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="counterfactual_dataset_generator",
+    version="0.1",
+    description="A counterfactual dataset generator to evaluate language model failures.",
+    author="SaferAI",
+    packages=find_packages(),
+)
