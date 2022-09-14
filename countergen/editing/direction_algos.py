6b2a02a727742bd07762eeef254021fc616d9f95
Fabien Roger <efab.roger@gmail.com>
2022-09-14 16:59:12 +0200

    Add another algorithm

--- countergen/editing/direction_algos.py
+++ countergen/editing/direction_algos.py
@@ -1,18 +1,15 @@
+from typing import List
 import torch
 from torch import nn
 from torchmetrics import HingeLoss
 
 from countergen.config import VERBOSE
 from countergen.editing.activation_ds import ActivationsDataset
-from countergen.editing.models import fit_model, get_bottlenecked_linear
+from countergen.editing.models import fit_model, get_bottlenecked_linear, get_bottlenecked_mlp
 from countergen.utils import maybe_tqdm, orthonormalize
 
 
-def inlp(
-    ds: ActivationsDataset,
-    n_dim: int = 8,
-    n_training_iters: int = 400,
-):
+def inlp(ds: ActivationsDataset, n_dim: int = 8, n_training_iters: int = 400) -> List[torch.Tensor]:
     working_ds = ds
     dirs = []
 
@@ -37,3 +34,17 @@ def inlp(
         if VERBOSE >= 1:
             g.set_postfix(**last_epoch_perf)
     return dirs
+
+
+def bottlenecked_mlp_span(ds: ActivationsDataset, n_dim: int = 8, n_training_iters: int = 400) -> List[torch.Tensor]:
+    tot_n_dims = ds.x_data.shape[-1]
+    output_dims = torch.max(ds.y_data).item() + 1
+    model = get_bottlenecked_mlp(tot_n_dims, output_dims, bottleneck_dim=n_dim)
+    last_epoch_perf = fit_model(model, ds, n_training_iters, loss_fn=HingeLoss())
+    if VERBOSE >= 2:
+        print(str(last_epoch_perf))
+
+    return [model[0].weight.detach()[i] for i in range(n_dim)]
+
+
+# TODO: Add the adversarial algorithm
