aa2eadeb9248f9a71e2f1ebffb9692aebf93b921
Fabien Roger <efab.roger@gmail.com>
2022-09-19 17:56:12 +0200

    Add api to generative

--- countergen/evaluation/generative_models.py
+++ countergen/evaluation/generative_models.py
@@ -2,12 +2,12 @@ from functools import lru_cache
 from math import exp
 from typing import TYPE_CHECKING, Callable, Iterable, List, Optional, Tuple
 
+import openai
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, BatchEncoding
-
-from countergen.types import Input, ModelEvaluator, Output, Performance
-from countergen.tools.utils import concat_dicts, get_device, get_gpt_tokenizer, remove_last_tok, unwrap_or
 from countergen.tools.math_utils import perplexity
+from countergen.tools.utils import concat_dicts, get_device, get_gpt_tokenizer, remove_last_tok, unwrap_or
+from countergen.types import Input, ModelEvaluator, Output, Performance
+from transformers import BatchEncoding, GPT2LMHeadModel, GPT2Tokenizer
 
 metrics = ["perplexity", "probability"]
 
@@ -24,14 +24,42 @@ def pt_to_generative_model(model: torch.nn.Module) -> GenerativeModel:
     tokenizer = get_gpt_tokenizer()
 
     def gen_model(inp, out) -> List[float]:
-
         tokens_inp = tokenizer(inp, return_tensors="pt").to(model.device)
-        inp_length = tokens_inp["input_ids"].shape[-1]
-        assert inp_length > 0, "Zero length input is forbidden"
-
         token_outs = [tokenizer(o, return_tensors="pt").to(model.device) for o in out]
 
         return get_correct_logprobs(tokens_inp, token_outs, model)
+
+    return gen_model
+
+
+def api_to_generative_model(openai_engine: str = "text-ada-001") -> GenerativeModel:
+    """Make a GenerativeModel that uses the openai api.
+
+    The GenerativeModel costs ~ len(input) * (sum of len(ouput)) tokens per call."""
+
+    def gen_model(inp, out) -> List[float]:
+        correct_log_probs_list = []
+        for o in out:
+            completion = openai.Completion.create(
+                engine=openai_engine,
+                prompt=inp + o,
+                max_tokens=1,
+                stream=False,
+                echo=True,
+                logprobs=5,
+            )["choices"][0]
+
+            token_logprobs = completion["logprobs"]["token_logprobs"]
+            token_offsets = completion["logprobs"]["text_offset"]
+
+            # Compute which tokens correspond to the output
+            # If token from input & output got merged (which should happen very rarely),
+            # takes into account the proability of the merged token.
+            n_toks = len(token_offsets)
+            start_of_output = max([i for i in range(n_toks) if token_offsets[i] <= len(inp)])
+
+            output_logprob = sum(token_logprobs[start_of_output:])
+            correct_log_probs_list.append(output_logprob)
 
     return gen_model
 
@@ -44,6 +72,8 @@ def get_evaluator_for_generative_model(model: GenerativeModel, metric: str = "pr
     def run(inp: Input, out: Output) -> Performance:
         if len(out) == 0:
             raise ValueError("Expected output should be provided for gpt models")
+        if len(inp) == 0:
+            raise ValueError("Empty inputs are forbidden for gpt models.")
 
         correct_log_probs_list = model(inp, out)
 
